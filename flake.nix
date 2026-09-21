c355464f9c89b98fa182dfb0e592198e0da14af9
= <NickFury7500@gmail.com>
2026-09-21 11:07:44 +0200

    Add package build thing

--- flake.nix
+++ flake.nix
@@ -45,5 +45,77 @@
             '';
           };
         });
+
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        nixpkgs.lib.optionalAttrs (system == "x86_64-linux") {
+          default = let
+            # rusty_v8 otherwise downloads this archive from its Cargo build script.
+            v8Archive = pkgs.fetchurl {
+              url = "https://github.com/denoland/rusty_v8/releases/download/v130.0.7/librusty_v8_release_x86_64-unknown-linux-gnu.a.gz";
+              hash = "sha256-pkdsuU6bAkcIHEZUJOt5PXdzK424CEgTLXjLtQ80t10=";
+            };
+          in
+          pkgs.rustPlatform.buildRustPackage {
+            pname = "limusic";
+            version = "0.8.0";
+            src = ./.;
+            doCheck = false;
+
+            cargoDeps = pkgs.rustPlatform.importCargoLock {
+              lockFile = ./Cargo.lock;
+            };
+
+            pnpmDeps = pkgs.fetchPnpmDeps {
+              pname = "limusic-ui";
+              version = "0.8.0";
+              src = ./ui;
+              pnpm = pkgs.pnpm;
+              fetcherVersion = 4;
+              hash = "sha256-uDdLf6rQryD/fDU4nHa9rjXu8GxYQkdfpu0KZMbi8IQ=";
+            };
+            pnpmRoot = "ui";
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              nodejs
+              pnpm
+              pnpmConfigHook
+              rustPlatform.cargoSetupHook
+              cargo
+              rustc
+            ];
+
+            buildInputs = with pkgs; [
+              mpv
+              webkitgtk_4_1
+              librsvg
+              openssl
+              gtk3
+              glib
+              libayatana-appindicator
+              dbus
+            ];
+
+            preBuild = ''
+              (cd ui && node node_modules/vite/bin/vite.js build)
+            '';
+
+            buildPhase = ''
+              runHook preBuild
+              cargo build --release --offline -p limusic-app
+            '';
+
+            installPhase = ''
+              install -Dm755 target/release/limusic-app $out/bin/limusic
+            '';
+
+            env = {
+              RUSTY_V8_ARCHIVE = v8Archive;
+            };
+          };
+        });
     };
 }
